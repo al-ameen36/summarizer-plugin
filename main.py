0d8138267f9e5341b3e259d5b7579e573ac47c58
al-ameen <muhdabdullahi36@gmail.com>
2025-01-01 17:51:17 +0100

    change AI model

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 genai.configure(api_key=os.environ["API_KEY"])
 
 app = FastAPI()
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash-exp")
 
 origins = ["*"]
 
